a3a10e28e18600ae34162bdc51a979a81f29df93
Duy <tranleduy1233@gmail.com>
2018-02-01 19:34:36 +0700

    lesson3

--- C_plus_plus_co_ban/Lesson3/Untitled3.cpp
+++ C_plus_plus_co_ban/Lesson3/Untitled3.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main(){
+	cout << "Hello everyone! My name's Duy" << "Nice to meet you" << endl;
+	cout << "I'm " << 23 << " years old." << endl;
 	
+	cout << "Line 1 \nLine 2\n";
+	cout << "\ttab\n";
+		
+	
+	system("pause");
 	return 0;
 }
