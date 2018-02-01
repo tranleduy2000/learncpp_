422dd90ef6b43ad11d261af056eda2f6c8b04e9c
Duy <tranleduy1233@gmail.com>
2018-02-01 19:50:36 +0700

    finish lesson6

---
+++ C_plus_plus_co_ban/lesson5/Untitled10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	const int year_of_birth = 1992;
+	year_of_birth = 10;
+	
+	
+	return 0;
+}
---
+++ C_plus_plus_co_ban/lesson6/lesson6_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	const int year_of_birth = 1992;
+	const int pi (3.14);
+	const int Pi { 3.14};
+	const int pI = 3.14;
+	
+	return 0;
+}
