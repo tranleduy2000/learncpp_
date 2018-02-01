f45318f6fde430865597ff2e3644c0fe66870358
Duy <tranleduy1233@gmail.com>
2018-02-01 19:46:46 +0700

    lesson5

---
+++ C_plus_plus_co_ban/lesson5/lesson5_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int value;
+	cout << "enter value: ";
+	cin >> value;
+	cout << "value is " << value;
+	
+
+	return 0;
+}
---
+++ C_plus_plus_co_ban/lesson5/lesson5_2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int day, month, year;
+	cin >> day >> month >> year;
+	cout << day << "/" << month << "/" << year;
+	return 0;
+}
